e91c3a390414ac749f4e25a7a32c9d31f63104a3
Michael Vergoz <m.vergoz@vergoz.ch>
2020-02-11 19:27:35 +0100

    Fix typo

--- test/rw-assignator.js
+++ test/rw-assignator.js
@@ -25,42 +25,42 @@ const schema = {
     }
 }
 
-describe('Read Write Assignator', function() {
-	it('should skip sub entries because write is false', function(done) {
-		const extract = fieldify.assign(schema, (user, dst, object, source) => {
+describe('Read Write Assignator', function () {
+    it('should skip sub entries because write is false', function (done) {
+        const extract = fieldify.assign(schema, (user, dst, object, source) => {
             dst["flags"] = object.$read === true ? "r" : "";
             dst["flags"] += object.$write === true ? "w" : "";
-            
+
             // do not follow the rest only if write is false
-            if(object.$write === false) return(false);
+            if (object.$write === false) return (false);
         });
 
         const jsons = JSON.stringify(extract);
-        if(jsons != '{"entry":{"flags":"w","subEntry1":{"flags":"rw"},"subEntry2":{"flags":"r","subEntry22":{"flags":"rw"}}}}') {
-            done("Entry is different: "+jsons);
+        if (jsons != '{"entry":{"flags":"w","subEntry1":{"flags":"rw"},"subEntry2":{"flags":"r","subEntry22":{"flags":"rw"}}}}') {
+            done("Entry is different: " + jsons);
         }
         else {
             done();
         }
-	});
-    
-	it('should skip sub entries because write & read is not true', function(done) {
-		const extract = fieldify.assign(schema, (user, dst, object, source) => {
+    });
+
+    it('should skip sub entries because write & read is not true', function (done) {
+        const extract = fieldify.assign(schema, (user, dst, object, source) => {
             dst["flags"] = object.$read === true ? "r" : "";
             dst["flags"] += object.$write === true ? "w" : "";
-            
+
             // do not follow the rest only if write is false
-            if(object.$write !== true && object.$read !== true) return(false);
+            if (object.$write !== true && object.$read !== true) return (false);
         });
 
         const jsons = JSON.stringify(extract);
-        if(jsons != '{"entry":{"flags":"w","subEntry1":{"flags":"rw"},"subEntry2":{"flags":"r","subEntry22":{"flags":"rw"}}}}') {
-            done("Entry is different: "+jsons);
+        if (jsons != '{"entry":{"flags":"w","subEntry1":{"flags":"rw"},"subEntry2":{"flags":"r","subEntry22":{"flags":"rw"}}}}') {
+            done("Entry is different: " + jsons);
         }
         else {
             done();
         }
-	});
+    });
 });
 
 
