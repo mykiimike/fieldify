860a295e261ed527ae96fdf932bef2db39bcd252
Michael Vergoz <m.vergoz@vergoz.ch>
2020-02-16 16:16:21 +0100

    Add compiler and first refacto of iterator

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const _utils = require("./lib/utils");
 const _fusion = require("./lib/fusion");
 const _assign = require("./lib/assign");
 const _iterator = require("./lib/iterator");
+const _compile = require("./lib/compile");
 
 module.exports = {
 	fusion: _fusion,
 	assign: _assign,
 	iterator: _iterator,
+	compile: _compile,
 	utils: _utils
 }
