d9861aac7abf55c16fb99f4199e9eeec229c4e2c
Michael Vergoz <m.vergoz@vergoz.ch>
2020-02-16 16:17:53 +0100

    Add iterator simple tests

---
+++ test/iterator-simple.js
@@ -0,0 +1,167 @@
+/*
+ * Iterator Basics
+ */
+const assert = require('assert');
+const fieldify = require("../index");
+
+describe('Iterator Basics', function () {
+
+    it('should get a valid return from a simple schema', function (done) {
+        const schema = {
+            first: {},
+            last: {}
+        }
+
+        const input = {
+            first: "Michael",
+            last: "Vergoz"
+        }
+
+        const hdl = fieldify.compile(schema)
+
+        const opts = {
+            handler: hdl,
+            input: input,
+
+            onAssign: ((current, next) => {
+                current.result[current.key] = "OK " + current.input;
+                next();
+            }),
+            onEnd: (iterator) => {
+                // compare output
+                const jsons = JSON.stringify(iterator.result);
+                if (jsons != '{"first":"OK Michael","last":"OK Vergoz"}') {
+                    done("Entry is different: " + jsons);
+                }
+                else {
+                    done();
+                }
+            },
+        }
+        fieldify.iterator(opts)
+    });
+
+
+    it('should get a valid return from a nested schema', function (done) {
+        const schema = {
+            name: {
+                first: {},
+                last: {}
+            }
+        }
+
+        const input = {
+            name: {
+                first: "Michael",
+                last: "Vergoz"
+            }
+        }
+
+        const hdl = fieldify.compile(schema)
+
+        const opts = {
+            handler: hdl,
+            input: input,
+
+            onAssign: ((current, next) => {
+                current.result[current.key] = "OK " + current.input;
+                next();
+            }),
+            
+            onEnd: (iterator) => {
+                // compare output
+                const jsons = JSON.stringify(iterator.result);
+                if (jsons != '{"name":{"first":"OK Michael","last":"OK Vergoz"}}') {
+                    done("Entry is different: " + jsons);
+                }
+                else {
+                    done();
+                }
+            },
+        }
+        fieldify.iterator(opts)
+    });
+
+    it('should get a valid return from a array schema', function (done) {
+        const schema = {
+            users: [{
+                first: {},
+                last: {}
+            }]
+        }
+
+        const input = {
+            users: [
+                {
+                    first: "AA1",
+                    last: "AB1"
+                },
+                {
+                    first: "BA2",
+                    last: "BB2"
+                },
+            ]
+        }
+
+        const hdl = fieldify.compile(schema)
+
+        const opts = {
+            handler: hdl,
+            input: input,
+
+            onAssign: ((current, next) => {
+                current.result[current.key] = "OK " + current.input;
+                next();
+            }),
+            onEnd: (iterator) => {
+                // compare output
+                const jsons = JSON.stringify(iterator.result);
+                if (jsons != '{"users":[{"first":"OK AA1","last":"OK AB1"},{"first":"OK BA2","last":"OK BB2"}]}') {
+                    done("Entry is different: " + jsons);
+                }
+                else {
+                    done();
+                }
+            },
+        }
+        fieldify.iterator(opts)
+    });
+
+
+
+
+    /*
+    it('should get a invalid return from a simple schema because of non nested input', function (done) {
+        const schema = {
+            first: {error: {}},
+            last: {}
+        }
+
+        const input = {
+           
+            first: "Michael",
+            last: "Vergoz"
+            
+        }
+
+        const hdl = fieldify.compile(schema)
+
+
+        const opts = {
+            handler: hdl,
+            input: input,
+            assign: () => {},
+            options: () => {},
+            end: (err) => {
+
+                done(err);
+            },
+        }
+        fieldify.iterator(opts)
+        
+    });
+*/
+
+});
+
+
