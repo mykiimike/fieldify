81ef21991362c297d3881c0a7e727679b7572052
Michael Vergoz <m.vergoz@vergoz.ch>
2020-05-19 11:30:15 +0200

    Add test for issue #1

---
+++ test/issue-1.js
@@ -0,0 +1,29 @@
+/*
+ * Issue #1
+ */
+const assert = require('assert');
+const fieldify = require("../index");
+
+describe('Issue #1', function () {
+
+    it('present of the issue', function (done) {
+        const schema = {
+            first: {
+                $test: 1
+            },
+        }
+
+        // first compilation
+        const compile = fieldify.compile(schema)
+
+        // second pass
+        fieldify.compile(schema)
+
+        if (!compile.schema.first.$_access) return (done("No access..."))
+        if (compile.schema.first.$_access.$_access) return (done("Issue is present"))
+
+        done()
+    });
+
+
+});
