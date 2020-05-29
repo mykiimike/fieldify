ac278bc60cd04a4ecf1ebe23d7399bb4f680d779
Michael Vergoz <m.vergoz@vergoz.ch>
2020-05-29 08:48:02 +0200

    Introduce the input schema comcept

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const assign = require('./lib/assign')
 const iterator = require('./lib/iterator')
 const compile = require('./lib/compile')
 const schema = require('./lib/schema')
+const input = require('./lib/input')
 const types = require('./lib/types')
 
 module.exports = {
   schema,
+  input,
   types,
   fusion,
   assign,
