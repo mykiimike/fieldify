63316e0e8915c1a2f8a7536923be314d40af0ed7
Michael VERGOZ <m.vergoz@vergoz.ch>
2023-06-03 23:32:07 +0200

    Add new test framework

---
+++ test/types.js
@@ -0,0 +1,141 @@
+
+
+const assert = require("assert")
+const fieldify = require("..")
+
+const stringTests = require("./types/string")
+const numberTests = require("./types/number")
+
+const bulks = [
+    {
+        ref: "STRING-B01",
+        tests: stringTests
+    },
+    {
+        ref: "NUMBER-B01",
+        tests: numberTests
+    },
+]
+
+
+
+// construct test in nested
+const nestedBulks = []
+for (let bulk of bulks) {
+    const newBulk = {
+        ref: bulk.ref,
+        tests: []
+    }
+
+    for (var test of bulk.tests) {
+        const newTest = { ...test }
+        newTest.schema = { insideNested: newTest.schema }
+        newTest.data = { insideNested: newTest.data }
+
+        if ("encode" in newTest)
+            newTest.encode = { insideNested: newTest.encode }
+        if ("strictDecode" in newTest)
+            newTest.strictDecode = { insideNested: newTest.strictDecode }
+        if ("decode" in newTest)
+            newTest.decode = { insideNested: newTest.decode }
+        if ("strictVerify" in newTest)
+            newTest.strictVerify = { insideNested: newTest.strictVerify }
+        if ("verify" in newTest)
+            newTest.verify = { insideNested: newTest.verify }
+
+        newBulk.tests.push(newTest)
+    }
+
+    nestedBulks.push(newBulk)
+}
+
+// construct test in array
+const arrayBulks = []
+for (let bulk of bulks) {
+    const newBulk = {
+        ref: bulk.ref,
+        tests: []
+    }
+
+    for (var test of bulk.tests) {
+        const newTest = { ...test }
+        newTest.schema = { insideArray: [newTest.schema] }
+        newTest.data = { insideArray: [newTest.data] }
+
+        if ("encode" in newTest)
+            newTest.encode = { insideArray: [newTest.encode] }
+        if ("strictDecode" in newTest)
+            newTest.strictDecode = { insideArray: [newTest.strictDecode] }
+        if ("decode" in newTest)
+            newTest.decode = { insideArray: [newTest.decode] }
+        if ("strictVerify" in newTest)
+            newTest.strictVerify = { insideArray: [newTest.strictVerify] }
+        if ("verify" in newTest)
+            newTest.verify = { insideArray: [newTest.verify] }
+
+        newBulk.tests.push(newTest)
+    }
+
+    arrayBulks.push(newBulk)
+}
+
+const bulkTests = [
+    { ref: "STD", type: "Standard", bulks },
+    { ref: "NEST", type: "Nested", bulks: nestedBulks },
+    { ref: "ARR", type: "Array", bulks: nestedBulks },
+]
+
+describe('Asynchronous types testing', function () {
+    const context = new fieldify.context
+
+    for (let bulkTest of bulkTests) {
+        describe(`Describing type ${bulkTest.type}`, function () {
+            for (let bulk of bulkTest.bulks) {
+                const bulkRef = `${bulk.ref}-${bulkTest.ref}`
+                describe(`Describing type bulk tests ${bulkRef}`, function () {
+
+                    function testingStrategy(test, strategy) {
+                        const testRef = `${bulkRef}-${test.ref}`
+                        it(`Testing ${strategy}() against ${testRef}: ${test.description}`, async () => {
+                            const schema = new fieldify.schema(context)
+                            const cerror = schema.compile(test.schema)
+
+                            if (cerror.errors.length > 0 && test.compileError !== true)
+                                throw Error(cerror.errors[0].message)
+                            else if (cerror.errors.length > 0 && test.compileError === true)
+                                return
+
+                            const ret = await schema[strategy](test.data)
+
+                            const errorKey = strategy + 'Error'
+                            const errorCode = errorKey in test ? test[errorKey] : test.error
+
+                            if (ret.error === true && errorCode !== true)
+                                throw Error(Object.values(ret.fields)[0])
+                            else if (ret.error !== true && errorCode === true)
+                                throw Error("Test should failed")
+                            else if (ret.error === true && errorCode === true)
+                                return
+                            else if (strategy in test)
+                                assert.deepEqual(ret.result, test[strategy]);
+
+                        });
+                    }
+
+                    for (let test of bulk.tests) {
+                        testingStrategy(test, "encode")
+                        testingStrategy(test, "strictDecode")
+                        testingStrategy(test, "decode")
+                        testingStrategy(test, "strictVerify")
+                        testingStrategy(test, "verify")
+                    }
+                });
+            }
+        })
+    }
+})
+
+
+
+
+
---
+++ test/types/number.js
@@ -0,0 +1,182 @@
+module.exports = [
+    {
+        ref: "T0001",
+        description: "Basic usage",
+        schema: { test: { $type: "number", $required: true } },
+        data: { test: "1" },
+        compileError: false,
+        error: false,
+        encode: { test: 1 },
+        strictDecode: { test: 1 },
+        decode: { test: 1 },
+        strictVerify: { test: 1 },
+        verify: { test: 1 }
+    },
+    {
+        ref: "T0002",
+        description: "Using $min",
+        schema: { test: { $type: "number", $min: 1, $required: true } },
+        data: { test: 0 },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+    {
+        ref: "T0003",
+        description: "Using $max",
+        schema: { test: { $type: "number", $max: 1, $required: true } },
+        data: { test: 2 },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+
+    // both mode
+    {
+        ref: "T0003",
+        description: "Using integer with both mode",
+        schema: { test: { $type: "number", $acceptedTypes: "both", $required: true } },
+        data: { test: 2 },
+        compileError: false,
+        error: false,
+        encode: { test: 2 },
+        strictDecode: { test: 2 },
+        decode: { test: 2 },
+        strictVerify: { test: 2 },
+        verify: { test: 2 }
+    },
+
+    {
+        ref: "T0004",
+        description: "Using float with both mode",
+        schema: { test: { $type: "number", $acceptedTypes: "both", $required: true } },
+        data: { test: 2.2 },
+        compileError: false,
+        error: false,
+        encode: { test: 2.2 },
+        strictDecode: { test: 2.2 },
+        decode: { test: 2.2 },
+        strictVerify: { test: 2.2 },
+        verify: { test: 2.2 }
+    },
+    {
+        ref: "T0005",
+        description: "Trigger error when NaN with both mode",
+        schema: { test: { $type: "number", $acceptedTypes: "both", $required: true } },
+        data: { test: "NaN" },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+
+    // integer mode
+    {
+        ref: "T0006",
+        description: "Using integer with integer mode",
+        schema: { test: { $type: "number", $acceptedTypes: "integer", $required: true } },
+        data: { test: 2 },
+        compileError: false,
+        error: false,
+        encode: { test: 2 },
+        strictDecode: { test: 2 },
+        decode: { test: 2 },
+        strictVerify: { test: 2 },
+        verify: { test: 2 }
+    },
+
+    {
+        ref: "T0007",
+        description: "Using float with integer mode",
+        schema: { test: { $type: "number", $acceptedTypes: "integer", $required: true } },
+        data: { test: 2.2 },
+        compileError: false,
+        error: false,
+        encode: { test: 2.2 },
+        strictDecode: { test: 2 },
+        decode: { test: 2 },
+        strictVerify: { test: 2 },
+        verify: { test: 2 }
+    },
+    {
+        ref: "T0008",
+        description: "Trigger error when NaN with integer mode",
+        schema: { test: { $type: "number", $acceptedTypes: "integer", $required: true } },
+        data: { test: "NaN" },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+
+    // float mode
+    {
+        ref: "T0006",
+        description: "Using integer with float mode",
+        schema: { test: { $type: "number", $acceptedTypes: "float", $required: true } },
+        data: { test: 2 },
+        compileError: false,
+        error: false,
+        encode: { test: 2 },
+        strictDecode: { test: 2 },
+        decode: { test: 2 },
+        strictVerify: { test: 2 },
+        verify: { test: 2 }
+    },
+
+    {
+        ref: "T0007",
+        description: "Using float with float mode",
+        schema: { test: { $type: "number", $acceptedTypes: "float", $required: true } },
+        data: { test: 2.2 },
+        compileError: false,
+        error: false,
+        encode: { test: 2.2 },
+        strictDecode: { test: 2.2 },
+        decode: { test: 2.2 },
+        strictVerify: { test: 2.2 },
+        verify: { test: 2.2 }
+    },
+    {
+        ref: "T0008",
+        description: "Trigger error when NaN with float mode",
+        schema: { test: { $type: "number", $acceptedTypes: "float", $required: true } },
+        data: { test: "NaN" },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+
+    // bigint mode
+    {
+        ref: "T0009",
+        description: "Using integer with bigint mode",
+        schema: { test: { $type: "number", $acceptedTypes: "bigint", $required: true } },
+        data: { test: "2135443545314314134035403841381408048" },
+        compileError: false,
+        error: false,
+        encode: { test: 2135443545314314134035403841381408048n },
+        strictDecode: { test: 2135443545314314134035403841381408048n },
+        decode: { test: 2135443545314314134035403841381408048n },
+        strictVerify: { test: 2135443545314314134035403841381408048n },
+        verify: { test: 2135443545314314134035403841381408048n }
+    },
+
+    {
+        ref: "T0010",
+        description: "Using float with bigint mode",
+        schema: { test: { $type: "number", $acceptedTypes: "bigint", $required: true } },
+        data: { test: 2.2 },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+    {
+        ref: "T0011",
+        description: "Trigger error when NaN with bigint mode",
+        schema: { test: { $type: "number", $acceptedTypes: "bigint", $required: true } },
+        data: { test: "NaN" },
+        compileError: false,
+        error: true,
+        encodeError: false
+    },
+
+]
